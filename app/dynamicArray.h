412692eba4bb7c8b72d53dcd1d2f8ce1176c4d22
MPalonek <mikepalonek@gmail.com>
2023-06-20 22:58:40 +0200

    Added copy/move constructors, added constructor to fill with default value

--- app/dynamicArray.h
+++ app/dynamicArray.h
@@ -5,6 +5,8 @@
 #include <string>
 #include <initializer_list>
 #include <stdexcept>
+#include <cassert>
+#include <algorithm>
 
 
 constexpr int initialCapacity{ 4 };
@@ -13,41 +15,52 @@ template <typename T>
 class dynamicArray
 {
 private:
-    int m_size{ 0 }; //How many elements are in array
-    int m_capacity{ initialCapacity }; //How big array is internally
+    int m_size{ 0 };                    // How many elements are in array
+    int m_capacity{ initialCapacity };  // How big array is internally
     T* m_arr{ nullptr };
 
     T* allocateArray();
     void expandArray();
 
 public:
-    dynamicArray(int size = initialCapacity);
+    dynamicArray(const int size = 0);
+    dynamicArray(const int size, const T value);
     dynamicArray(std::initializer_list<T> iList);
     ~dynamicArray();
-    dynamicArray(const dynamicArray&) = delete; // to avoid shallow copy
+    dynamicArray(const dynamicArray& other);
     dynamicArray& operator=(const dynamicArray& other);
+    dynamicArray(dynamicArray&& other) noexcept;
+    dynamicArray& operator=(dynamicArray&& other) noexcept;
 
     T& operator[](const int index);
     const T& operator[](const int index) const;
-    void append(T elem); //insert at the end
-    void insert(T elem, int index);
+    void append(const T elem); //insert at the end
+    void insert(const T elem, const int index);
     int size() const;
 };
 
 template <typename T>
-dynamicArray<T>::dynamicArray(int size) : m_capacity{ size }
+dynamicArray<T>::dynamicArray(const int size) : m_size{ size }
 {
-    if (size <= 0)
-        throw std::invalid_argument("Array size is 0 or less.");
+    if (m_size < 0)
+        throw std::invalid_argument("Array size cannot be negative.");
+    if (m_size > m_capacity)
+        m_capacity = m_size + 2;
     m_arr = allocateArray();
+}
+
+template <typename T>
+dynamicArray<T>::dynamicArray(const int size, const T value) : dynamicArray(size)
+{
+    std::fill_n(m_arr, size, value);
 }
 
 template <typename T>
 dynamicArray<T>::dynamicArray(std::initializer_list<T> iList) : dynamicArray(static_cast<int>(iList.size()))
 {
-    for (const auto& elem : iList)
+    for (int i{ 0 }; const auto& elem : iList)
     {
-        this->append(elem);
+        m_arr[i++] = elem;
     }
 }
 
@@ -55,6 +68,15 @@ template <typename T>
 dynamicArray<T>::~dynamicArray()
 {
     delete[] m_arr;
+}
+
+template <typename T>
+dynamicArray<T>::dynamicArray(const dynamicArray& other) : dynamicArray(other.m_size)
+{
+    for (int i = 0; i < other.m_size; i++)
+    {
+        m_arr[i] = other[i];
+    }
 }
 
 template<typename T>
@@ -83,14 +105,39 @@ dynamicArray<T>& dynamicArray<T>::operator=(const dynamicArray& other)
 }
 
 template <typename T>
+dynamicArray<T>::dynamicArray(dynamicArray&& other) noexcept
+{
+    m_arr = other.m_arr;
+    m_capacity = other.m_capacity;
+    m_size = other.m_size;
+    
+    other.m_arr = nullptr;
+}
+
+template <typename T>
+dynamicArray<T>& dynamicArray<T>::operator=(dynamicArray&& other) noexcept
+{
+    delete[] m_arr;
+    m_arr = other.m_arr;
+    m_capacity = other.m_capacity;
+    m_size = other.m_size;
+
+    other.m_arr = nullptr;
+    return *this;
+}
+
+template <typename T>
 T* dynamicArray<T>::allocateArray()
 {
-    return new T[m_capacity];
+    return new T[m_capacity]{};
 }
 
 template <typename T>
 void dynamicArray<T>::expandArray()
 {
+    //assert(m_capacity != 0 && "Array capacity is zero during expansion!");
+    if (m_capacity == 0)
+        throw std::runtime_error("Array capacity is zero during expansion!");
     m_capacity *= 2;
     T* tempArr = allocateArray();
 
@@ -122,7 +169,7 @@ const T& dynamicArray<T>::operator[](const int index) const
 }
 
 template <typename T>
-void dynamicArray<T>::append(T elem)
+void dynamicArray<T>::append(const T elem)
 {
     // out of memory, expand array
     if (m_size == m_capacity)
@@ -134,7 +181,7 @@ void dynamicArray<T>::append(T elem)
 }
 
 template <typename T>
-void dynamicArray<T>::insert(T elem, int index)
+void dynamicArray<T>::insert(const T elem, const int index)
 {
     if ((index < 0) || (index > m_size))
         throw std::out_of_range("Out of array bounds");
--- test/dynamicArrayTest.cpp
+++ test/dynamicArrayTest.cpp
@@ -6,19 +6,30 @@
 // dynamicArray<int> tests
 // ---------------------------------------------------------------------
 
-TEST(dynamicArraySuite, CreateArrayInt)
+TEST(dynamicArrayIntSuite, CreateArray)
 {
 	dynamicArray<int> arr;
 	EXPECT_EQ(arr.size(), 0);
 }
 
-TEST(dynamicArraySuite, CreateArraySize10Int)
+TEST(dynamicArrayIntSuite, CreateArraySize10)
 {
 	dynamicArray<int> arr(10);
-	EXPECT_EQ(arr.size(), 0);
+	EXPECT_EQ(arr.size(), 10);
 }
 
-TEST(dynamicArraySuite, InitilizerListInt)
+TEST(dynamicArrayIntSuite, CreateArraySize5WithValues)
+{
+	dynamicArray<int> arr(5, 24);
+	EXPECT_EQ(arr[0], 24);
+	EXPECT_EQ(arr[1], 24);
+	EXPECT_EQ(arr[2], 24);
+	EXPECT_EQ(arr[3], 24);
+	EXPECT_EQ(arr[4], 24);
+	EXPECT_EQ(arr.size(), 5);
+}
+
+TEST(dynamicArrayIntSuite, InitilizerList)
 {
 	dynamicArray<int> arr{ 50, 40, 30, 20, 10 };
 	EXPECT_EQ(arr[0], 50);
@@ -28,38 +39,20 @@ TEST(dynamicArraySuite, InitilizerListInt)
 	EXPECT_EQ(arr[4], 10);
 }
 
-TEST(dynamicArraySuite, AppendElementsInt)
+TEST(dynamicArrayIntSuite, CopyConstructArray)
 {
-	dynamicArray<int> arr;
-	arr.append(10);
-	arr.append(20);
-	arr.append(30);
-	EXPECT_EQ(arr[0], 10);
-	EXPECT_EQ(arr[1], 20);
-	EXPECT_EQ(arr[2], 30);
+	dynamicArray<int> arr{ 100, 200, 300, 400, 500, 600 };
+	dynamicArray<int> arr2(arr);
+	EXPECT_EQ(arr2[0], 100);
+	EXPECT_EQ(arr2[1], 200);
+	EXPECT_EQ(arr2[2], 300);
+	EXPECT_EQ(arr2[3], 400);
+	EXPECT_EQ(arr2[4], 500);
+	EXPECT_EQ(arr2[5], 600);
+	EXPECT_EQ(arr2.size(), arr.size());
 }
 
-TEST(dynamicArraySuite, InsertElementInt)
-{
-	dynamicArray<int> arr{ 10, 20, 40 };
-	arr.insert(30, 2);
-	EXPECT_EQ(arr[0], 10);
-	EXPECT_EQ(arr[1], 20);
-	EXPECT_EQ(arr[2], 30);
-	EXPECT_EQ(arr[3], 40);
-}
-
-TEST(dynamicArraySuite, ReplaceExistingElementInt)
-{
-	dynamicArray<int> arr{ 30, 20, 0, 50 };
-	arr[2] = 10;
-	EXPECT_EQ(arr[0], 30);
-	EXPECT_EQ(arr[1], 20);
-	EXPECT_EQ(arr[2], 10);
-	EXPECT_EQ(arr[3], 50);
-}
-
-TEST(dynamicArraySuite, AssignExistingArrToAnotherInt)
+TEST(dynamicArrayIntSuite, CopyAssignArray)
 {
 	dynamicArray<int> arr{ 30, 40, 50, 60 };
 	dynamicArray<int> arr2{ 1, 2, 3, 4 };
@@ -71,7 +64,18 @@ TEST(dynamicArraySuite, AssignExistingArrToAnotherInt)
 	EXPECT_EQ(arr.size(), arr2.size());
 }
 
-TEST(dynamicArraySuite, AssignExistingArrToAnotherDiffSizeInt)
+TEST(dynamicArrayIntSuite, CopyAssignArraySmallerSize)
+{
+	dynamicArray<int> arr{ 3, 4, 5 };
+	dynamicArray<int> arr2{ 20, 21, 22, 23, 24 };
+	arr2 = arr;
+	EXPECT_EQ(arr2[0], 3);
+	EXPECT_EQ(arr2[1], 4);
+	EXPECT_EQ(arr2[2], 5);
+	EXPECT_EQ(arr.size(), arr2.size());
+}
+
+TEST(dynamicArrayIntSuite, CopyAssignArrayBiggerSize)
 {
 	dynamicArray<int> arr{ 30, 40, 50, 60, 70, 80 };
 	dynamicArray<int> arr2{ 1, 2 };
@@ -85,23 +89,88 @@ TEST(dynamicArraySuite, AssignExistingArrToAnotherDiffSizeInt)
 	EXPECT_EQ(arr.size(), arr2.size());
 }
 
+TEST(dynamicArrayIntSuite, MoveConstructArray)
+{
+	dynamicArray<int> arr{ 4, 4, 7, 8 };
+	dynamicArray<int> arr2 = std::move(arr);
+	EXPECT_EQ(arr2[0], 4);
+	EXPECT_EQ(arr2[1], 4);
+	EXPECT_EQ(arr2[2], 7);
+	EXPECT_EQ(arr2[3], 8);
+	EXPECT_EQ(arr2.size(), 4);
+}
+
+TEST(dynamicArrayIntSuite, MoveAssignArray)
+{
+	dynamicArray<int> arr{ 5, 5, 8, 8 };
+	dynamicArray<int> arr2{ 1 };
+	arr2 = std::move(arr);
+	EXPECT_EQ(arr2[0], 5);
+	EXPECT_EQ(arr2[1], 5);
+	EXPECT_EQ(arr2[2], 8);
+	EXPECT_EQ(arr2[3], 8);
+	EXPECT_EQ(arr2.size(), 4);
+}
+
+TEST(dynamicArrayIntSuite, AppendElements)
+{
+	dynamicArray<int> arr;
+	arr.append(10);
+	arr.append(20);
+	arr.append(30);
+	EXPECT_EQ(arr[0], 10);
+	EXPECT_EQ(arr[1], 20);
+	EXPECT_EQ(arr[2], 30);
+}
+
+TEST(dynamicArrayIntSuite, InsertElement)
+{
+	dynamicArray<int> arr{ 10, 20, 40 };
+	arr.insert(30, 2);
+	EXPECT_EQ(arr[0], 10);
+	EXPECT_EQ(arr[1], 20);
+	EXPECT_EQ(arr[2], 30);
+	EXPECT_EQ(arr[3], 40);
+}
+
+TEST(dynamicArrayIntSuite, ReplaceExistingElement)
+{
+	dynamicArray<int> arr{ 30, 20, 0, 50 };
+	arr[2] = 10;
+	EXPECT_EQ(arr[0], 30);
+	EXPECT_EQ(arr[1], 20);
+	EXPECT_EQ(arr[2], 10);
+	EXPECT_EQ(arr[3], 50);
+}
+
 // ---------------------------------------------------------------------
 // dynamicArray<double> tests
 // ---------------------------------------------------------------------
 
-TEST(dynamicArraySuite, CreateArrayDouble)
+TEST(dynamicArrayDoubleSuite, CreateArray)
 {
 	dynamicArray<double> arr;
 	EXPECT_EQ(arr.size(), 0);
 }
 
-TEST(dynamicArraySuite, CreateArraySize10Double)
+TEST(dynamicArrayDoubleSuite, CreateArraySize10)
 {
 	dynamicArray<double> arr(10);
-	EXPECT_EQ(arr.size(), 0);
+	EXPECT_EQ(arr.size(), 10);
 }
 
-TEST(dynamicArraySuite, InitilizerListDouble)
+TEST(dynamicArrayDoubleSuite, CreateArraySize5WithValues)
+{
+	dynamicArray<double> arr(5, 2.4);
+	EXPECT_EQ(arr[0], 2.4);
+	EXPECT_EQ(arr[1], 2.4);
+	EXPECT_EQ(arr[2], 2.4);
+	EXPECT_EQ(arr[3], 2.4);
+	EXPECT_EQ(arr[4], 2.4);
+	EXPECT_EQ(arr.size(), 5);
+}
+
+TEST(dynamicArrayDoubleSuite, InitilizerList)
 {
 	dynamicArray<double> arr{ 5.0, 4.0, 3.0, 2.0, 1.0 };
 	EXPECT_EQ(arr[0], 5.0);
@@ -111,37 +180,20 @@ TEST(dynamicArraySuite, InitilizerListDouble)
 	EXPECT_EQ(arr[4], 1.0);
 }
 
-TEST(dynamicArraySuite, AppendElementsDouble)
+TEST(dynamicArrayDoubleSuite, CopyConstructArray)
 {
-	dynamicArray<double> arr;
-	arr.append(1.0);
-	arr.append(2.5);
-	arr.append(3.75);
-	EXPECT_EQ(arr[0], 1.0);
-	EXPECT_EQ(arr[1], 2.5);
-	EXPECT_EQ(arr[2], 3.75);
+	dynamicArray<double> arr{ 0.0, 11.1, 22.2, 33.3, 44.4, 55.5 };
+	dynamicArray<double> arr2(arr);
+	EXPECT_EQ(arr2[0], 0.0);
+	EXPECT_EQ(arr2[1], 11.1);
+	EXPECT_EQ(arr2[2], 22.2);
+	EXPECT_EQ(arr2[3], 33.3);
+	EXPECT_EQ(arr2[4], 44.4);
+	EXPECT_EQ(arr2[5], 55.5);
+	EXPECT_EQ(arr2.size(), arr.size());
 }
 
-TEST(dynamicArraySuite, InsertElementsDouble)
-{
-	dynamicArray<double> arr{ 0.1, 0.2, 0.4 };
-	arr.insert(0.3, 2);
-	EXPECT_EQ(arr[0], 0.1);
-	EXPECT_EQ(arr[1], 0.2);
-	EXPECT_EQ(arr[2], 0.3);
-	EXPECT_EQ(arr[3], 0.4);
-}
-
-TEST(dynamicArraySuite, ReplaceExistingElementDouble)
-{
-	dynamicArray<double> arr{ 1.5, 3.0, 0.0 };
-	arr[2] = 3.3333;
-	EXPECT_EQ(arr[0], 1.5);
-	EXPECT_EQ(arr[1], 3.0);
-	EXPECT_EQ(arr[2], 3.3333);
-}
-
-TEST(dynamicArraySuite, AssignExistingArrToAnotherDouble)
+TEST(dynamicArrayDoubleSuite, CopyAssignArray)
 {
 	dynamicArray<double> arr{ 3.0, 4.0, 5.0, 6.0 };
 	dynamicArray<double> arr2{ 0.1, 0.2, 0.3, 0.4 };
@@ -153,7 +205,18 @@ TEST(dynamicArraySuite, AssignExistingArrToAnotherDouble)
 	EXPECT_EQ(arr.size(), arr2.size());
 }
 
-TEST(dynamicArraySuite, AssignExistingArrToAnotherDiffSizeDouble)
+TEST(dynamicArrayDoubleSuite, CopyAssignArraySmallerSize)
+{
+	dynamicArray<double> arr{ 0.5, 0.6, 0.7 };
+	dynamicArray<double> arr2{ -2.0, -1.0, 0.0, 1.0, 2.0 };
+	arr2 = arr;
+	EXPECT_EQ(arr2[0], 0.5);
+	EXPECT_EQ(arr2[1], 0.6);
+	EXPECT_EQ(arr2[2], 0.7);
+	EXPECT_EQ(arr.size(), arr2.size());
+}
+
+TEST(dynamicArrayDoubleSuite, CopyAssignArrayBiggerSize)
 {
 	dynamicArray<double> arr{ 3.0, 4.0, 5.0, 6.0, 7.0, 8.0 };
 	dynamicArray<double> arr2{ 0.1, 0.2 };
@@ -165,4 +228,57 @@ TEST(dynamicArraySuite, AssignExistingArrToAnotherDiffSizeDouble)
 	EXPECT_EQ(arr2[4], 7.0);
 	EXPECT_EQ(arr2[5], 8.0);
 	EXPECT_EQ(arr.size(), arr2.size());
+}
+
+TEST(dynamicArrayDoubleSuite, MoveConstructArray)
+{
+	dynamicArray<double> arr{ 4.0, 4.0, 7.0, 8.0 };
+	dynamicArray<double> arr2 = std::move(arr);
+	EXPECT_EQ(arr2[0], 4.0);
+	EXPECT_EQ(arr2[1], 4.0);
+	EXPECT_EQ(arr2[2], 7.0);
+	EXPECT_EQ(arr2[3], 8.0);
+	EXPECT_EQ(arr2.size(), 4);
+}
+
+TEST(dynamicArrayDoubleSuite, MoveAssignArray)
+{
+	dynamicArray<double> arr{ 5.2, 5.2, 8.2, 8.2 };
+	dynamicArray<double> arr2{ 1.5 };
+	arr2 = std::move(arr);
+	EXPECT_EQ(arr2[0], 5.2);
+	EXPECT_EQ(arr2[1], 5.2);
+	EXPECT_EQ(arr2[2], 8.2);
+	EXPECT_EQ(arr2[3], 8.2);
+	EXPECT_EQ(arr2.size(), 4);
+}
+
+TEST(dynamicArrayDoubleSuite, AppendElements)
+{
+	dynamicArray<double> arr;
+	arr.append(1.0);
+	arr.append(2.5);
+	arr.append(3.75);
+	EXPECT_EQ(arr[0], 1.0);
+	EXPECT_EQ(arr[1], 2.5);
+	EXPECT_EQ(arr[2], 3.75);
+}
+
+TEST(dynamicArrayDoubleSuite, InsertElements)
+{
+	dynamicArray<double> arr{ 0.1, 0.2, 0.4 };
+	arr.insert(0.3, 2);
+	EXPECT_EQ(arr[0], 0.1);
+	EXPECT_EQ(arr[1], 0.2);
+	EXPECT_EQ(arr[2], 0.3);
+	EXPECT_EQ(arr[3], 0.4);
+}
+
+TEST(dynamicArrayDoubleSuite, ReplaceExistingElement)
+{
+	dynamicArray<double> arr{ 1.5, 3.0, 0.0 };
+	arr[2] = 3.3333;
+	EXPECT_EQ(arr[0], 1.5);
+	EXPECT_EQ(arr[1], 3.0);
+	EXPECT_EQ(arr[2], 3.3333);
 }
